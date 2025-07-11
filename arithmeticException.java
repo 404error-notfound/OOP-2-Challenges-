ba537db0bf81783ee230537e7268ab1ecffa92a5
Angela Omondi <omondiangela426@gmail.com>
2025-07-11 23:18:30 +0300

    Add files via upload
    divide by zero exception (30/06/2025)

---
+++ arithmeticException.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class arithmeticException {
+    public static void main (String[]args){
+        int num1=100;
+        int num2=0;
+        double answer=num1/num2;
+        System.out.println("Answer = "+answer);
+    }
+}
